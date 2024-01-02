0bcb2b6d22e0260abec3e6e30323f4c58e59db4f
Ghaith Husrieh <Ghais.hs@gmail.com>
2024-01-02 05:17:07 +0300

    Refactor logging format for better readability and additional context

--- decorators/log_decorators.py
+++ decorators/log_decorators.py
@@ -6,26 +6,32 @@ logger = logging.getLogger(__name__)
 
 def log_api_view(view_func):
     @wraps(view_func)
-    def wrapper(*args, **kwargs):
-        logger.info(f'View function {view_func.__name__} was called from {view_func.__module__}')
+    def wrapper(request, *args, **kwargs):
+        user = request.user
+        user_info = f"User:({user.username}({user.id}))" if user.is_authenticated else "User:Anonymous User"
+        logger.info(
+            f'{user_info} | View function {view_func.__name__} was called from {view_func.__module__} | Method:{request.method} | Path:{request.path}'
+        )
         try:
-            response = view_func(*args, **kwargs)
+            response = view_func(request, *args, **kwargs)
             if response.status_code >= 200 and response.status_code < 300:
                 logger.info(
-                    f"View function {view_func.__name__} returned successful response with status code: {response.status_code}")
+                    f"{user_info} | View function {view_func.__name__} returned successful response with status code: {response.status_code}")
             elif response.status_code >= 400 and response.status_code < 500:
                 logger.warning(
-                    f"View function {view_func.__name__} returned client error response with status code: {response.status_code}")
+                    f"{user_info} | View function {view_func.__name__} returned client error response with status code: {response.status_code}")
             elif response.status_code >= 500:
                 logger.error(
-                    f"View function {view_func.__name__} returned server error response with status code: {response.status_code}")
+                    f"{user_info} | View function {view_func.__name__} returned server error response with status code: {response.status_code}")
             else:
                 logger.info(
-                    f"View function {view_func.__name__} returned response with status code: {response.status_code}")
+                    f"{user_info} | View function {view_func.__name__} returned response with status code: {response.status_code}")
         except Exception as e:
-            logger.exception(f"View function {view_func.__name__} raised an exception: {str(e)}")
+            logger.exception(
+                f"{user_info} | View function {view_func.__name__} raised an exception: {str(e)}")
             raise
-        logger.debug(f"View function {view_func.__name__} response data: {response.data}")
+        logger.debug(
+            f"{user_info} | View function {view_func.__name__} response data: {response.data}")
         return response
     return wrapper
 
@@ -36,7 +42,7 @@ def log_function(func):
         logger.info(f'Function {func.__name__} was called from {func.__module__}')
         try:
             result = func(*args, **kwargs)
-            logger.info(f'Function {func.__name__} executed successfully')
+            logger.info(f'Function {func.__name__} finished execution | RESULT:{result}')
         except Exception as e:
             logger.exception(f'Function {func.__name__} raised and exception: {str(e)}')
             raise
